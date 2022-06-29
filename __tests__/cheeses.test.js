f4e0a8c9b8ddf6b1e0cc19f7f9230f03e6451c69
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-29 11:42:44 -0700

    test written and red for /api/v1/cheeses/:id

--- __tests__/cheeses.test.js
+++ __tests__/cheeses.test.js
@@ -23,4 +23,17 @@ describe('cheese routes', () => {
     expect(res.body.length).toEqual(15);
     expect(res.body[0]).toEqual(cheese);
   });
+
+  it('/api/v1/cheeses/:id', async () => {
+    const res = await request(app).get('/api/v1/cheese/1');
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: 'American',
+      description: 'American is a creamy, smooth cheese made from blending natural cheeses. It comes in several forms including individually wrapped cheese slices, small pre-sliced blocks and large blocks. It melts well.',
+      url: 'https://www.organicauthority.com/.image/t_share/MTU5MzI5OTc3NjUzMzM5MzUw/shutterstock_288297071.jpg',
+      pairs: 'pairs with chicken broth',
+      smells: false
+    });
+  });
 });
