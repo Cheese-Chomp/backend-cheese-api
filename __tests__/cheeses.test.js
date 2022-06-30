bbac738e2571e0b275b95c5f400b0178abae8564
Mary Martinez <mary.diana.martinez@gmail.com>
2022-06-29 17:27:53 -0700

    POST /cheeses route is working and passing test

--- __tests__/cheeses.test.js
+++ __tests__/cheeses.test.js
@@ -24,7 +24,7 @@ describe('cheese routes', () => {
     expect(res.body[0]).toEqual(cheese);
   });
 
-  it('/api/v1/cheeses/:id', async () => {
+  it('/api/v1/cheeses/:id should return cheese details', async () => {
     const res = await request(app).get('/api/v1/cheeses/1');
     expect(res.status).toEqual(200);
     expect(res.body).toEqual({
@@ -35,5 +35,23 @@ describe('cheese routes', () => {
       pairs: 'pairs with chicken broth',
       smells: false
     });
+  });
+
+  it('POST /api/v1/cheeses should add a new cheese to the table', async () => {
+    const res = await request(app).post('/api/v1/cheeses').send({
+      name: 'String cheese',
+      description: 'Fun, delicious snack',
+      url: 'http://static1.squarespace.com/static/5424b7b6e4b0bec306d9c41f/5424d2dbe4b0fab9148fa16f/5646caaae4b0e0e27be5cc8b/1585002570101/cesars+string.jpg?format=1500w',
+      pairs: 'pairs with grape broth',
+      smells: false
+    });
+
+    expect(res.status).toEqual(200);
+
+    const res2 = await request(app).get(`/api/v1/cheeses/${res.body.id}`);
+    expect(res2.status).toEqual(200);
+    expect(res2.body.id).toEqual(expect.any(String));
+    expect(res2.body.name).toEqual('String cheese');
+
   });
 });
